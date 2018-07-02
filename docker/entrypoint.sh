5ea60dd9e47229fd97cf5eae064039cb23d26ee7
anarcher <anarcher@gmail.com>
2018-07-02 10:57:10 +0900

    Initial commit

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This entrypoint has 2 modes: if any argument is provided to `docker run`, the
+# arguments are passed directly to sebak Otherwise, it just starts a node with the
+# environment file
+if [ $# -gt 1 ]; then
+    # Argument mode
+    ./sebak $@
+else
+    # Node mode
+    ./sebak genesis ${SEBAK_GENESIS_BLOCK}
+    ./sebak node --log-level debug
+fi
